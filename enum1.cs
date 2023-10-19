42f5c489ca201c35fb842834bacef063561fce8f
mehmetdamkaci <109851073+mehmetdamkaci@users.noreply.github.com>
2023-10-19 12:23:11 +0300

    Add files via upload

---
+++ enum1.cs
@@ -0,0 +1,31 @@
+namespace wpf_1
+{
+    public class Enums
+    {
+
+        public enum Mehmet
+        {
+			Damkacı,
+			PC_4259,
+			AkıllıSistem
+			
+        }
+		
+		public enum Kağan{
+			oğuz,
+			çoban,
+			PC_4258,
+			İleriOtonomi
+		}
+
+        public enum Kadir
+        {
+			Yavuz,
+			Akıncı,
+			PC_4255,
+			DerinÖğrenme
+        }
+    }
+
+    
+}
