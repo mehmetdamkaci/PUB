4fb7b443dca12576329f05ba5f47da32ef4b43e7
mehmetdamkaci <109851073+mehmetdamkaci@users.noreply.github.com>
2023-10-25 10:58:33 +0300

    Add files via upload

---
+++ CZR_Paket.cs
@@ -0,0 +1,59 @@
+namespace wpf_1
+{
+    public class Enums
+    {		
+		public enum CZR_PAKET
+		{
+			CZR_SURECLER = 1,
+			CZR_SISTEMLER = 2,			
+			CZR_NAVIGASYON= 3,
+			CZR_GPS = 4,
+			CZR_KONUM = 5		
+		}		
+
+		public enum SUREC_CZR
+		{
+			SUREC_1 = 1,
+			SUREC_2 = 2,
+			SUREC_3 = 3,
+			SUREC_4 = 4,
+			SUREC_5 = 5
+		}
+
+		public enum SISTEM_CZR
+		{
+			SISTEM_1 = 1,
+			SISTEM_2 = 2,
+			SISTEM_3 = 3,
+			SISTEM_4 = 4,
+			SISTEM_5 = 5
+		}
+
+		public enum NVG_CZR
+		{
+			NAVIGASYON_1 = 1,
+			NAVIGASYON_2 = 2,
+			NAVIGASYON_3 = 3,
+			NAVIGASYON_4 = 4,
+			NAVIGASYON_5 = 5			
+		}
+
+		public enum GPS_CZR
+		{
+			GPS_1 = 1,
+			GPS_2 = 2,
+			GPS_3 = 3,
+			GPS_4 = 4,
+			GPS_5 = 5
+		}
+
+		public enum KONUM_CZR
+		{
+			KONUM_1 = 1,
+			KONUM_2 = 2,
+			KONUM_3 = 3,
+			KONUM_4 = 4,
+			KONUM_5 = 5			
+		}
+    }    
+}
